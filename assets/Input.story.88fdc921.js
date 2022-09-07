e1ea67e546a937732042d82ba6c6e26f910f46d0
effervescentia <ben.l.teichman@gmail.com>
2022-09-07 07:59:28 +0000

    Deploying to docs from @ voiceflow/react-chat@6eb6f7ec1d73869e2854dbfd5d9ec5638f4a0bd9 🚀

--- assets/Input.story.88fdc921.js
+++
@@ -1,2 +0,0 @@
-import{I as t}from"./index.98748734.js";import{j as r}from"./jsx-runtime.43489d4f.js";import"./theme.6ba8ee0e.js";import"./iframe.d3ffc062.js";const m={title:"Core/Input",component:t},o=e=>r(t.Controlled,{...e}),c=o.bind({}),n=o.bind({});n.args={placeholder:"Message\u2026"};export{c as Default,n as Placeholder,m as default};
-//# sourceMappingURL=Input.story.88fdc921.js.map
