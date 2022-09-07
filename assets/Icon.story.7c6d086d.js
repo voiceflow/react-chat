ae0d159539a791748e162f838262ed2c9e6b316c
effervescentia <ben.l.teichman@gmail.com>
2022-09-07 07:23:10 +0000

    Deploying to docs from @ voiceflow/react-chat@f076f5e4b02c15a21118d85849dc39b5c7efb7f0 🚀

--- assets/Icon.story.7c6d086d.js
+++
@@ -1,2 +0,0 @@
-import{I as o}from"./index.938ac06c.js";import{j as s}from"./jsx-runtime.7a0368ad.js";import"./theme.0277f88c.js";import"./iframe.86483f83.js";const m={title:"Core/Icon",component:o},n=t=>s(o,{...t}),r=n.bind({});r.args={svg:"launch",css:{color:"pink",height:50,width:50}};export{r as Default,m as default};
-//# sourceMappingURL=Icon.story.7c6d086d.js.map
