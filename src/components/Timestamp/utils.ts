4e621d6521d08601707b860bfa5294ba4f43ca1d
Ben Teichman <ben@voiceflow.com>
2022-09-26 12:12:13 -0400

    feat: format 12-hour timestamp

--- src/components/Timestamp/utils.ts
+++ src/components/Timestamp/utils.ts
@@ -2,5 +2,5 @@ export const formatTimestamp = (date: Date) => {
   const hours = date.getHours();
   const minutes = date.getMinutes();
 
-  return `${hours}:${minutes >= 10 ? '' : '0'}${minutes} ${hours >= 12 ? 'pm' : 'am'}`;
+  return `${hours % 12 || 12}:${minutes >= 10 ? '' : '0'}${minutes} ${hours >= 12 ? 'pm' : 'am'}`;
 };
