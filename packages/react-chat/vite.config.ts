96a58e60e2e9e4c1937f1021de22aa5774baa9c4
Tyler Han <tylerhan97@gmail.com>
2022-11-04 15:26:49 -0400

    feat: caching by commit sha (CT-000) (#31)

--- packages/react-chat/vite.config.ts
+++ packages/react-chat/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   },
   base: '',
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: path.resolve(__dirname, 'dist', process.env.CIRCLE_SHA1 || 'latest'),
     lib: {
       entry: path.resolve(__dirname, 'index.html'),
       name: 'voiceflow-chat',
--- packages/widget/typings/env.d.ts
+++ packages/widget/typings/env.d.ts
@@ -1,10 +1,2 @@
 /// <reference types="vite-plugin-svgr/client" />
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_WIDGET_URL: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
