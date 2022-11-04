997b8aa3271be2fdc7b67535e357ee5f9bf02909
Tyler Han <tylerhan97@gmail.com>
2022-11-04 15:14:48 -0400

    fix: chill out with the env load

--- packages/widget/vite.config.ts
+++ packages/widget/vite.config.ts
@@ -8,7 +8,7 @@ export const createPlugins = (rootDir = __dirname): PluginOption[] => [
 ];
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd());
   const VITE_WIDGET_URL = env.VITE_WIDGET_URL.replace('CIRCLE_SHA1', process.env.CIRCLE_SHA1 || 'latest');
 
   return {
