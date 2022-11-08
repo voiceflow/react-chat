17eb09f1910f939a214bcabe0037b161595119a0
Tyler Han <tylerhan97@gmail.com>
2022-11-08 15:49:31 -0500

    feat: use context instead of drilling (CT-1156) (#33)

--- packages/react-chat/src/components/SystemResponse/index.tsx
+++ packages/react-chat/src/components/SystemResponse/index.tsx
@@ -1,5 +1,8 @@
-import type { RuntimeAction, SendMessage } from '@/common';
+import { useContext } from 'react';
+
+import type { RuntimeAction } from '@/common';
 import Button from '@/components/Button';
+import { RuntimeAPIContext } from '@/contexts';
 import { useAutoScroll } from '@/hooks';
 
 import { MessageType } from './constants';
@@ -22,11 +25,11 @@ export interface SystemResponseProps {
   messages: MessageProps[];
   actions?: ResponseActionProps[];
   isLast?: boolean;
-  send?: SendMessage;
-  onEnd?: VoidFunction;
 }
 
-const SystemResponse: React.FC<SystemResponseProps> = ({ send, avatar, timestamp, messages, actions = [], isLast, onEnd }) => {
+const SystemResponse: React.FC<SystemResponseProps> = ({ avatar, timestamp, messages, actions = [], isLast }) => {
+  const runtime = useContext(RuntimeAPIContext);
+
   const { showIndicator, visibleMessages, complete } = useAnimatedMessages({
     messages,
     isLast,
@@ -40,20 +43,18 @@ const SystemResponse: React.FC<SystemResponseProps> = ({ send, avatar, timestamp
     <>
       {visibleMessages.map((message, index) => (
         <SystemMessage
-          send={send}
           message={message}
           withImage={!showIndicator && index === visibleMessages.length - 1}
           avatar={avatar}
           timestamp={timestamp}
           key={index}
-          onEnd={onEnd}
         />
       ))}
 
       {isLast && complete && !!actions.length && (
         <Actions>
           {actions.map(({ name, request }, index) => (
-            <Button variant="secondary" onClick={() => send?.(name, request)} key={index}>
+            <Button variant="secondary" onClick={() => runtime?.send(name, request)} key={index}>
               {name}
             </Button>
           ))}
