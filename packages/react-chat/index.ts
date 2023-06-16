d4688d0fe6616d6bbc4c02b90877fb511a501401
Tyler Han <tylerhan97@gmail.com>
2023-06-16 13:13:53 -0700

    fix: export runtime context

--- packages/react-chat/index.ts
+++ packages/react-chat/index.ts
@@ -1,5 +1,6 @@
 export * from './src/common';
 export * from './src/components';
+export * from './src/contexts';
 export * from './src/hooks';
 export * from './src/styles';
 export * from './src/types/turn';
