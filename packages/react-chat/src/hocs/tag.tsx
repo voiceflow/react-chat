527cd08c8695a429a2ff99841e7fd34d69eae007
Ben Teichman <ben@voiceflow.com>
2023-07-10 14:57:39 -0400

    build: fix build errors

--- packages/react-chat/src/hocs/tag.tsx
+++ packages/react-chat/src/hocs/tag.tsx
@@ -5,11 +5,11 @@ export const tagFactory =
   (
     prefix: string
   ): {
-    <Tag extends keyof JSX.IntrinsicElements>(tag: Tag, suffix?: string): (props: React.ComponentProps<Tag>) => React.ReactNode;
-    <Props extends any>(component: React.ComponentType<Props>, suffix?: string): (props: Props) => React.ReactNode;
+    <Tag extends keyof JSX.IntrinsicElements>(tag: Tag, suffix?: string): (props: React.ComponentProps<Tag>) => React.ReactElement;
+    <Props extends any>(component: React.ComponentType<Props>, suffix?: string): (props: Props) => React.ReactElement;
   } =>
   (tagOrComponent: keyof JSX.IntrinsicElements | React.ComponentType<any>, suffix?: string) => {
     return forwardRef((props: any, ref) =>
       createElement(tagOrComponent, { ...props, ref, className: clsx(suffix ? `${prefix}--${suffix}` : prefix, props.className) })
-    );
+    ) as any;
   };
