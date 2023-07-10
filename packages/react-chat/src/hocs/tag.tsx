b4c9006961ec594754951ca3e474c4201085fdd2
Josh <josh@voiceflow.com>
2023-07-10 14:19:52 -0400

    fix: update tagFactory to allow refs (CT-000)

--- packages/react-chat/src/hocs/tag.tsx
+++ packages/react-chat/src/hocs/tag.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { createElement } from 'react';
+import { createElement, forwardRef } from 'react';
 
 export const tagFactory =
   (
@@ -8,6 +8,8 @@ export const tagFactory =
     <Tag extends keyof JSX.IntrinsicElements>(tag: Tag, suffix?: string): (props: React.ComponentProps<Tag>) => React.ReactElement;
     <Props extends any>(component: React.ComponentType<Props>, suffix?: string): (props: Props) => React.ReactElement;
   } =>
-  (tagOrComponent: keyof JSX.IntrinsicElements | React.ComponentType<any>, suffix?: string) =>
-  (props: any) =>
-    createElement(tagOrComponent, { ...props, className: clsx(suffix ? `${prefix}--${suffix}` : prefix, props.className) });
+  (tagOrComponent: keyof JSX.IntrinsicElements | React.ComponentType<any>, suffix?: string) => {
+    return forwardRef((props: any, ref) =>
+      createElement(tagOrComponent, { ...props, ref, className: clsx(suffix ? `${prefix}--${suffix}` : prefix, props.className) })
+    );
+  };
