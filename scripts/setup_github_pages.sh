9d39ea4d13b8ce3735ad97988e65f168453b1a95
Ben Teichman <ben@voiceflow.com>
2023-07-18 14:14:24 -0400

    feat: add script for setting up github pages

---
+++ scripts/setup_github_pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+git checkout --orphan pages
+git reset .
+git add -f packages/react-chat/dist
+git add -f packages/widget/dist
+cp scripts/template.html index.html
+git add index.html
+git commit -m "initial" --no-verify
+git push
+git checkout -
+git clean index.html
