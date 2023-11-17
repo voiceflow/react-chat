5867b652b6de5ba7b42d5c094af365ebb38be77c
Tyler Han <tylerhan97@gmail.com>
2023-11-17 09:12:49 -0500

    fix: test mocks

--- packages/react-chat/__mocks__/@voiceflow/stitches-react.ts
+++ packages/react-chat/__mocks__/@voiceflow/stitches-react.ts
@@ -2,6 +2,7 @@ import { vi } from 'vitest';
 
 export const createStitches = vi.fn().mockReturnValue({
   styled: vi.fn().mockImplementation((el) => el),
+  keyframes: vi.fn(),
 });
 
 export const keyframes = vi.fn();
