ef8e87336a3be92bfd5c7d8538c8cb5defdaa1e8
Nina Bondar <nina.bondar@voiceflow.com>
2023-12-19 13:36:04 -0500

    fix: apply white-space breaks to user messages only

--- packages/react-chat/src/components/Message/styled.ts
+++ packages/react-chat/src/components/Message/styled.ts
@@ -6,7 +6,6 @@ export const tag = tagFactory(ClassName.MESSAGE);
 
 export const Container = styled(tag('div'), {
   display: 'inline-block',
-  whiteSpace: 'break-spaces',
   boxSizing: 'border-box',
   padding: '10px 14px',
   borderRadius: '$1',
