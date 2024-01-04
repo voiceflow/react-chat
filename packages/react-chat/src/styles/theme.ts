b851da310f862f5cd8ca711cbbbb096563fe91bf
Nina Bondar <nina.bondar@voiceflow.com>
2024-01-04 15:08:11 -0500

    fix: storybook build

--- packages/react-chat/src/styles/theme.ts
+++ packages/react-chat/src/styles/theme.ts
@@ -23,65 +23,68 @@ export interface FontOptions {
   height?: BaseCSS['lineHeight'] | Token<typeof Font['LINE_HEIGHTS']>;
 }
 
-export const { styled, config, keyframes, theme, createTheme, getCssText } = createStitches({
-  ...(__USE_SHADOW_ROOT__ && { root: shadowRoot }),
+export const { styled, config, keyframes, theme, createTheme, getCssText } = createStitches(
+  {
+    ...(__USE_SHADOW_ROOT__ && { root: shadowRoot }),
 
-  theme: {
-    colors: Color.PALETTE,
-    shadows: Color.SHADOWS,
+    theme: {
+      colors: Color.PALETTE,
+      shadows: Color.SHADOWS,
 
-    space: {
-      1: '4px',
-      2: '6px',
-      3: '12px',
-      4: '16px',
-      5: '20px',
-      6: '24px',
+      space: {
+        1: '4px',
+        2: '6px',
+        3: '12px',
+        4: '16px',
+        5: '20px',
+        6: '24px',
+      },
+      sizes: {
+        xxs: '16px',
+        xs: '24px',
+        sm: '36px',
+        md: '42px',
+        lg: '56px',
+        xl: '60px',
+        xxl: '72px',
+      },
+
+      fonts: {
+        default: Font.FAMILY,
+      },
+      fontSizes: Font.SIZES,
+      fontWeights: Font.WEIGHTS,
+      lineHeights: Font.LINE_HEIGHTS,
+
+      radii: {
+        1: '8px',
+        2: '10px',
+        round: '50%',
+      },
     },
-    sizes: {
-      xxs: '16px',
-      xs: '24px',
-      sm: '36px',
-      md: '42px',
-      lg: '56px',
-      xl: '60px',
-      xxl: '72px',
+    media: {
+      mobile: '(max-width: 768px)',
     },
 
-    fonts: {
-      default: Font.FAMILY,
-    },
-    fontSizes: Font.SIZES,
-    fontWeights: Font.WEIGHTS,
-    lineHeights: Font.LINE_HEIGHTS,
+    utils: {
+      anim: (animations: { (): string }[]) => ({
+        animation: animations.map((animation) => `${animation} ${ANIMATION_DURATION}ms`).join(', '),
+      }),
 
-    radii: {
-      1: '8px',
-      2: '10px',
-      round: '50%',
+      trans: (properties: Array<keyof CSSPropertiesHyphen>) => ({
+        transition: createTransition(properties),
+      }),
+
+      typo: ({ size = Font.SIZES[2], weight = Font.WEIGHTS[1], height = Font.LINE_HEIGHTS[1] }: FontOptions) => ({
+        fontFamily: '$default',
+        fontSize: size,
+        fontWeight: weight,
+        lineHeight: height,
+      }),
     },
   },
-  media: {
-    mobile: '(max-width: 768px)',
-  },
-
-  utils: {
-    anim: (animations: { (): string }[]) => ({
-      animation: animations.map((animation) => `${animation} ${ANIMATION_DURATION}ms`).join(', '),
-    }),
-
-    trans: (properties: Array<keyof CSSPropertiesHyphen>) => ({
-      transition: createTransition(properties),
-    }),
-
-    typo: ({ size = Font.SIZES[2], weight = Font.WEIGHTS[1], height = Font.LINE_HEIGHTS[1] }: FontOptions) => ({
-      fontFamily: '$default',
-      fontSize: size,
-      fontWeight: weight,
-      lineHeight: height,
-    }),
-  },
-});
+  __USE_SHADOW_ROOT__
+);
 
 interface ThemeOverrides {
   color?: string | undefined;
