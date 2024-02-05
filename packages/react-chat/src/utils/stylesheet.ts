202de02dca34c84668aa75857565a54838c4976d
Nina Bondar <nina.bondar@voiceflow.com>
2024-02-05 15:27:47 -0500

    fix: wip

--- packages/react-chat/src/utils/stylesheet.ts
+++ packages/react-chat/src/utils/stylesheet.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 import { Assistant } from '@/common';
-import { shadowRoot } from '@/shadow';
+//import { shadowRoot } from '@/shadow';
 
 // used to add stylesheets dynamically, resolves when loaded
 export const addStyleSheetURL = async (url: string) => {
@@ -13,7 +13,7 @@ export const addStyleSheetURL = async (url: string) => {
 
   link.rel = 'stylesheet';
   link.href = url;
-  shadowRoot.appendChild(link);
+  //shadowRoot.appendChild(link);
 
   await load;
 };
