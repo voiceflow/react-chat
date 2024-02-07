c08411309e29a8ffbdb02649d9bf64dacd6f726d
Nina Bondar <nina.bondar@voiceflow.com>
2024-02-06 22:51:49 -0500

    fix: autostart works

--- packages/react-chat/src/utils/session.ts
+++ packages/react-chat/src/utils/session.ts
@@ -15,6 +15,8 @@ const getStorageSession = (storage: Storage, projectID: string): SessionOptions 
 };
 
 const setStorageSession = (storage: Storage, projectID: string, options: SessionOptions) => {
+  console.log('setStorageSession', options);
+
   storage.setItem(getSessionKey(projectID), JSON.stringify(options));
 
   return options;
@@ -45,6 +47,8 @@ export const getSession = (persistence: ChatPersistence, projectID: string, user
 };
 
 export const saveSession = (persistence: ChatPersistence, projectID: string, session: SessionOptions): void => {
+  console.log('Save settinon', session);
+
   broadcast({ type: BroadcastType.SAVE_SESSION, payload: session });
 
   if (persistence === ChatPersistence.LOCAL_STORAGE) {
