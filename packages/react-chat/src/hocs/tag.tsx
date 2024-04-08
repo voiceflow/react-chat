6f301ecb76798728336e309acefbd6856df9b5a0
Ben Teichman <ben@voiceflow.com>
2024-04-08 11:49:38 -0400

    chore: linting errors

--- packages/react-chat/src/hocs/tag.tsx
+++ packages/react-chat/src/hocs/tag.tsx
@@ -6,7 +6,7 @@ export const tagFactory =
     prefix: string
   ): {
     <Tag extends keyof JSX.IntrinsicElements>(tag: Tag, suffix?: string): (props: React.ComponentProps<Tag>) => React.ReactElement;
-    <Props extends any>(component: React.ComponentType<Props>, suffix?: string): (props: Props) => React.ReactElement;
+    <Props>(component: React.ComponentType<Props>, suffix?: string): (props: Props) => React.ReactElement;
   } =>
   (tagOrComponent: keyof JSX.IntrinsicElements | React.ComponentType<any>, suffix?: string) => {
     return forwardRef((props: any, ref) =>
