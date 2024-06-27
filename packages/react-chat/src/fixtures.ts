8c4f408657d4c6e0b9c225cc35467e15161f7e1b
Nina Bondar <nina.bondar@voiceflow.com>
2024-06-27 15:01:19 -0400

    fix: word break in card descriptions && image mock url

--- packages/react-chat/src/fixtures.ts
+++ packages/react-chat/src/fixtures.ts
@@ -1,4 +1,3 @@
 export const VF_ICON = 'https://cm4-production-assets.s3.amazonaws.com/1668625026913-vf-icon.png';
 
-export const MOCK_IMAGE_ID = 'Mg2Zmw4rtoI';
-export const MOCK_IMAGE = `https://source.unsplash.com/${MOCK_IMAGE_ID}/248x150`;
+export const MOCK_IMAGE = `https://picsum.photos/seed/9/300/200`
