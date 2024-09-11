8845288f941e30c9942eb6b161c821c3333ea363
effervescentia <ben.l.teichman@gmail.com>
2024-09-11 17:57:02 +0000

    Deploying to docs from @ voiceflow/react-chat@78dacb0aad231fee5b3a82ae372191e6e0da844c 🚀

--- assets/Footer.story-Df16YWME.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-DRTy3Uxn.js";import{F as a,C as l}from"./index-CC6KWeNj.js";import"./index-BBkUAzwr.js";import"./index-cJifsxF3.js";import"./tag-DslUz7EE.js";import"./animation-I98MwHEk.js";import"./index-CgswGT5k.js";import"./variants-BbyesEZ0.js";import"./index-D-TPDnAa.js";import"./index-BqsSZhcB.js";import"./index-Dg1kBsK5.js";import"./index-BWp-D-Q6.js";import"./top-caret-CFqa3nzJ.js";import"./index-CcHdfiB1.js";import"./functional-ii7S4iDS.js";import"./index-CrQ5MIvt.js";import"./index-2PWa66_n.js";import"./index-BZJ2QIV7.js";import"./index-GBvVpXIh.js";import"./index-CocICL3R.js";import"./_createSet-zuc-p8Rl.js";import"./_baseToString-BV3LLLfG.js";import"./is-plain-object-BrWfeXm7.js";import"./index-CLMQvdPU.js";import"./index-BNFyRyue.js";import"./index-HQ-V58eR.js";import"./index-PqR-_bA4.js";import"./index-CLC4ExZJ.js";const O={title:"Components/Chat/Footer",component:a,argTypes:{onStart:{action:"onStart"},onSend:{action:"send"}},args:{hasEnded:!1,withWatermark:!1},render:h=>e.jsx(l.Container,{children:e.jsx(a,{...h})})},r={},t={args:{hasEnded:!0}},o={args:{withWatermark:!0}};var s,m,i;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(i=(m=r.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};var p,n,c;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
-  args: {
-    hasEnded: true
-  }
-}`,...(c=(n=t.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var d,u,g;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
-  args: {
-    withWatermark: true
-  }
-}`,...(g=(u=o.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};export{t as Ended,r as Running,o as WithWatermark,O as default};
