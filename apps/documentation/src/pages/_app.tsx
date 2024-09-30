1c02d2380bb27dbec63d3ac68388993a2fbb241b
Mikaal Naik <matthewnaik@gmail.com>
2024-09-30 12:40:50 -0400

    wip: good place to reset to

--- apps/documentation/src/pages/_app.tsx
+++ apps/documentation/src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import '@/styles/globals.css';
 
+import { Button } from '@voiceflow/ui/next';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import { DismissableLayersGlobalProvider } from 'react-dismissable-layers';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [active, setActive] = useState(0);
+
+  return (
+    <DismissableLayersGlobalProvider>
+      <div>
+        <Button label="howdy" />
+        <Component {...pageProps} />
+      </div>
+    </DismissableLayersGlobalProvider>
+  );
 }
