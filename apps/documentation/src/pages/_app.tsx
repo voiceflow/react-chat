046c0f105d2934aa0ddb64875fd27c39cc0a4527
Mikaal Naik <matthewnaik@gmail.com>
2024-09-30 13:43:25 -0400

    feat: let's see if this work

--- apps/documentation/src/pages/_app.tsx
+++ apps/documentation/src/pages/_app.tsx
@@ -1,19 +1,11 @@
 import '@/styles/globals.css';
 
-import { Button } from '@voiceflow/ui/next';
 import type { AppProps } from 'next/app';
-import { useState } from 'react';
-import { DismissableLayersGlobalProvider } from 'react-dismissable-layers';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [active, setActive] = useState(0);
-
   return (
-    <DismissableLayersGlobalProvider>
-      <div>
-        <Button label="howdy" />
-        <Component {...pageProps} />
-      </div>
-    </DismissableLayersGlobalProvider>
+    <div>
+      <Component {...pageProps} />
+    </div>
   );
 }
