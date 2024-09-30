19b8fa17b730ac13c02036c4f6322981a412ccd7
Ben Teichman <ben@voiceflow.com>
2024-09-30 17:35:17 -0400

    feat: avoid adding a new build pipeline

--- packages/chat/vite.package.config.ts
+++ packages/chat/vite.package.config.ts
@@ -3,19 +3,18 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-import { createPlugins } from './vite.config';
+import { createPlugins } from './vite.widget.config';
 
 export default defineConfig({
-  define: {
-    __USE_SHADOW_ROOT__: false,
-    'process.env': {},
-  },
   build: {
     outDir: path.resolve(__dirname, 'build'),
     lib: {
-      entry: path.resolve(__dirname, 'src', 'package.entry.ts'),
+      entry: {
+        main: path.resolve(__dirname, './src/main.ts'),
+        ui: path.resolve(__dirname, './src/ui.ts'),
+      },
       name: 'voiceflow-chat',
-      fileName: (format) => `index.${format}.js`,
+      fileName: (format, entry) => `${entry}.${format}.js`,
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
@@ -28,5 +27,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), dts(), ...createPlugins()],
+  plugins: [react(), dts({ tsconfigPath: 'tsconfig.build.json' }), ...createPlugins()],
 });
