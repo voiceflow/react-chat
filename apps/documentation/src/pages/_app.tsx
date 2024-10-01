eb5deeb8d78f86645c78361d35e032a5e721b1d2
Mikaal Naik <matthewnaik@gmail.com>
2024-09-30 20:21:32 -0400

    feat: UI export to docs (DX-000) (#193)
    Co-authored-by: Gilly Barr <gillyb@gmail.com>
    Co-authored-by: Ben Teichman <ben@voiceflow.com>

--- apps/documentation/src/pages/_app.tsx
+++ apps/documentation/src/pages/_app.tsx
@@ -3,5 +3,9 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
