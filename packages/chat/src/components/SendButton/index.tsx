9838789da99a00fcd0f8e291626eb39d25ac5f61
Gilly Barr <gillyb@gmail.com>
2024-10-03 14:24:36 -0400

    fix: build issue

--- packages/chat/src/components/SendButton/index.tsx
+++ packages/chat/src/components/SendButton/index.tsx
@@ -1,15 +1,20 @@
+import { type ComponentPropsWithRef, forwardRef } from 'react';
+
 import Button from '../Button';
 import { SendIcon } from './SendIcon';
 import { sendButtonStyle, sendIconStyle } from './styles.css';
 
-interface SendButtonProps {
+interface SendButtonProps extends ComponentPropsWithRef<'button'> {
   disabled?: boolean;
+  testID?: string;
 }
 
-const SendButton: React.FC<SendButtonProps> = (props) => (
-  <Button className={sendButtonStyle({ disabled: !!props.disabled })}>
-    <SendIcon className={sendIconStyle({ disabled: !!props.disabled })} />
-  </Button>
+const SendButton: React.FC<SendButtonProps> = forwardRef<HTMLButtonElement, SendButtonProps>(
+  ({ disabled, testID, ...props }, ref) => (
+    <Button ref={ref} className={sendButtonStyle({ disabled: !!disabled })} data-testid={testID} {...props}>
+      <SendIcon className={sendIconStyle({ disabled: !!disabled })} />
+    </Button>
+  )
 );
 
 export default SendButton;
