886c34ebc2d327e23017e37a82583da439a36371
Mikaal Naik <matthewnaik@gmail.com>
2024-10-04 09:26:37 -0400

    feat: add docs

--- packages/chat/src/stories.ts
+++ packages/chat/src/stories.ts
@@ -2,4 +2,5 @@ export * as Avatar from './components/Avatar/Avatar.story';
 export * as Button from './components/Button/Button.story';
 export * as ColorTheme from './components/ColorTheme/ColorTheme.story';
 export * as FeedbackButton from './components/FeedbackButton/FeedbackButton.story';
+export * as Message from './components/Message/Message.story';
 export * as SendButton from './components/SendButton/SendButton.story';
