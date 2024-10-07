4c23a3168deb21e8e08dd26713aaca882f7a1184
Gilly Barr <gillyb@gmail.com>
2024-10-07 17:48:12 -0400

    feat: add Launcher button to the mix (COR-3541) (#211)

---
+++ packages/chat/src/components/Launcher/ChevronIcon.tsx
@@ -0,0 +1,11 @@
+import type { SVGProps } from 'react';
+export const ChevronIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M21.6368 13.1181C22.1575 12.5806 23.0244 12.5584 23.5731 13.0685C24.1219 13.5785 24.1446 14.4277 23.6238 14.9652L16.9232 21.8819C16.3792 22.4434 15.4641 22.4387 14.9262 21.8715L8.36623 14.9549C7.85136 14.412 7.88324 13.5631 8.43744 13.0587C8.99164 12.5544 9.85829 12.5856 10.3732 13.1285L15.4743 18.5001C15.7726 18.7649 16.2274 18.7649 16.5256 18.5L21.6368 13.1181Z"
+      fill="currentColor"
+    />
+  </svg>
+);
--- packages/chat/src/components/Launcher/index.tsx
+++ packages/chat/src/components/Launcher/index.tsx
@@ -1,8 +1,10 @@
 import type { MouseEventHandler } from 'react';
 import React from 'react';
 
-import launch from './launch.svg';
-import { Button } from './styled';
+import Button from '../Button';
+import { ChevronIcon } from './ChevronIcon';
+import { PlayIcon } from './PlayIcon';
+import { launcherIconStyles, launcherLabelStyles, launcherStyles } from './styles.css';
 
 export interface LauncherProps {
   /**
@@ -12,15 +14,31 @@ export interface LauncherProps {
   image?: string | undefined;
 
   /**
+   * A label that will be displayed on the launcher button.
+   * If empty or undefined then the user will only see the icon.
+   */
+  label?: string | undefined;
+
+  /**
+   * Is the Chat widget opened ?
+   * If it's opened then we only show the close icon.
+   */
+  isOpen: boolean;
+
+  /**
    * A callback that will be executed when the button is clicked.
    */
   onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
-const Launcher: React.FC<LauncherProps> = ({ image, onClick }) => {
+const Launcher: React.FC<LauncherProps> = ({ image, isOpen, label, onClick }) => {
+  const withLabel = !!label?.length;
   return (
-    <Button onClick={onClick}>
-      <img src={image || launch} alt="launch" />
+    <Button className={launcherStyles({ withLabel })} onClick={onClick}>
+      {withLabel && <div className={launcherLabelStyles}>{label}</div>}
+      {image && !isOpen && <img src={image} className={launcherIconStyles({ withLabel })} alt="launch" />}
+      {!image && !isOpen && <PlayIcon className={launcherIconStyles({ withLabel })} />}
+      {isOpen && <ChevronIcon className={launcherIconStyles()} />}
     </Button>
   );
 };
