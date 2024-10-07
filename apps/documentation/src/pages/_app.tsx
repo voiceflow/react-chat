7383f823a8c78c76e32a244328333efc82b67507
Mikaal Naik <matthewnaik@gmail.com>
2024-10-07 14:53:27 -0400

    fix: revert back app

--- apps/documentation/src/pages/_app.tsx
+++ apps/documentation/src/pages/_app.tsx
@@ -1,8 +1,9 @@
-import '@/styles/globals.css';
-import '@voiceflow/chat/dist/style.css';
-
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <div>{typeof window !== 'undefined' && <Component {...pageProps} />}</div>;
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
