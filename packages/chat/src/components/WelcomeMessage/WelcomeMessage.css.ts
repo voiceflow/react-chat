8093c541c21de40f7d4508fb7a04014244c9a515
Mikaal Naik <matthewnaik@gmail.com>
2024-10-10 13:12:21 -0400

    fix: wip. just need to rebase once font work is merged

---
+++ packages/chat/src/components/WelcomeMessage/WelcomeMessage.css.ts
@@ -0,0 +1,27 @@
+import { style } from '@vanilla-extract/css';
+
+import { COLORS } from '@/styles/colors';
+
+export const welcomeMessageContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '48px 16px 24px 16px',
+});
+
+export const avatarContainer = style({
+  marginBottom: 16,
+});
+
+export const welcomeMessageTitle = style({
+  // fontFamily: FAMILY
+  margin: '0 0 4px 0',
+  fontSize: '22px',
+  color: COLORS.NEUTRAL_DARK[900],
+});
+
+export const welcomeMessageDescription = style({
+  margin: 0,
+  fontSize: '14px',
+  color: COLORS.NEUTRAL_DARK[100],
+});
