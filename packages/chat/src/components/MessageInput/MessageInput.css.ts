45dbadc76cbf9cf4170cb3d350dbad566a43a1e3
Mikaal Naik <matthewnaik@gmail.com>
2024-10-16 13:10:26 -0400

    feat: looking decent

--- packages/chat/src/components/MessageInput/MessageInput.css.ts
+++ packages/chat/src/components/MessageInput/MessageInput.css.ts
@@ -5,37 +5,57 @@ import { FAMILY } from '@/styles/font';
 import { transition } from '@/styles/transitions';
 
 export const inputContainer = style({
-  position: 'relative',
-  width: '100%',
-});
-
-export const input = style({
   borderRadius: '100px',
   width: '100%',
-  outline: 'none',
   display: 'block',
   boxSizing: 'border-box',
   padding: '13px 8px 11px 20px',
-  border: `1px solid ${COLORS.NEUTRAL_LIGHT[100]}`,
-  fontFamily: FAMILY,
-  lineHeight: '20px',
+  outline: `1px solid ${COLORS.NEUTRAL_LIGHT[100]}`,
+  position: 'relative',
   backgroundColor: COLORS.white,
   boxShadow: '0px 3px 4px 0px rgba(0, 0, 0, 0.02), 0px 8px 42px -16px rgba(0, 0, 0, 0.08)',
-
+  border: '2px solid transparent',
   transition: transition(['border-color', 'outline', 'box-shadow']),
   selectors: {
     '&:hover': {
-      border: `1px solid ${COLORS.NEUTRAL_LIGHT[200]}`,
+      outline: `1px solid ${COLORS.NEUTRAL_LIGHT[200]}`,
     },
     '&:focus': {},
-    '&:focus-visible': {
-      // outline: '1px solid red',
+    '&:focus-within': {
+      outline: '1px solid transparent',
+      border: `2px solid ${COLORS.ACCENT[500]}`,
     },
+
     '&::placeholder': {
       // transform: 'transla  teY(1px)',
       // marginTop: '2px',
       color: COLORS.NEUTRAL_DARK[100],
       fontSize: '14px',
+    },
+  },
+});
+
+export const input = style({
+  outline: 'none',
+  border: 'none',
+  borderColor: 'transparent',
+  outlineColor: 'transparent',
+  fontFamily: FAMILY,
+  lineHeight: '20px',
+});
+
+export const mockFocus = style({
+  position: 'absolute',
+  top: -1,
+  left: -1,
+  right: -1,
+  bottom: -1,
+  borderRadius: '100px',
+  pointerEvents: 'none',
+  transition: transition(['border-color']),
+  selectors: {
+    [`${input}:focus-within ~ &`]: {
+      border: `2px solid ${COLORS.ACCENT[500]}`,
     },
   },
 });
