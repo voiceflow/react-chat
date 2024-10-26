20c7c0649c312bc116d4330ad3a7797fe767acee
Gilly Barr <gillyb@gmail.com>
2024-10-25 23:28:06 -0400

    fix: add animation keyframes to typing indicator

--- packages/chat/src/components/TypingIndicator/styles.css.ts
+++ packages/chat/src/components/TypingIndicator/styles.css.ts
@@ -1,7 +1,13 @@
-import { style } from '@vanilla-extract/css';
+import { keyframes, style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
 import { SIZES } from '@/styles/sizes';
+
+const ANIMATION_DURATION = 1000;
+
+const pulseAnimation = keyframes({
+  '50%': { opacity: 1 },
+});
 
 export const indicatorContainer = style({
   display: 'inline-flex',
@@ -13,10 +19,9 @@ export const dot = recipe({
     height: 8,
     margin: '0 2px',
     borderRadius: SIZES.radius.round,
-    backgroundColor: '#adadb3',
+    backgroundColor: '#adadb3', // TODO: Should this be in the COLORS array
     opacity: 0.2,
-    // TODO: Get keyFrames here
-    animation: '',
+    animation: `${pulseAnimation} ${ANIMATION_DURATION}ms`,
     animationIterationCount: 'infinite',
   },
 
