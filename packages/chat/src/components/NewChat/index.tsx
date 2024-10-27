a5fed4f37a75edd02619fddd06805d1f55808fa4
Gilly Barr <gillyb@gmail.com>
2024-10-27 11:40:08 -0400

    feat: add Prompt to NewChat component and adjust styles

--- packages/chat/src/components/NewChat/index.tsx
+++ packages/chat/src/components/NewChat/index.tsx
@@ -1,12 +1,16 @@
 import clsx from 'clsx';
-import { useRef, useState } from 'react';
+import { useContext, useMemo, useRef, useState } from 'react';
 
 import { ClassName } from '@/constants';
+import { RuntimeStateAPIContext, RuntimeStateContext } from '@/contexts';
+import { RenderMode } from '@/dtos/RenderOptions.dto';
 import type { Nullish } from '@/types';
+import { chain } from '@/utils/functional';
 
 import mockAvatar from '../../assets/blank-image.png';
-import { Header, type HeaderProps } from '../Header';
+import { Header, type HeaderActionProps, type HeaderProps } from '../Header';
 import { type INewFooter, NewFooter } from '../NewFooter';
+import { Prompt } from '../Prompt';
 import { ScrollToBottom } from '../ScrollToBottom';
 import { type IWelcomeMessage, WelcomeMessage } from '../WelcomeMessage';
 import { chatContainer, dialogContainer } from './NewChat.css';
@@ -46,12 +50,46 @@ export const NewChat: React.FC<INewChat> = ({
   description,
   avatar,
   hasEnded,
+  onMinimize,
+  onEnd,
   extraLinkText,
   extraLinkUrl,
   children,
+  audioInterface,
 }) => {
   // const [chatMessages, setChatMessages] = useState(messages ?? []);
   const [newMessage, setNewMessage] = useState('');
+  const [hasAlert, setAlert] = useState(false);
+
+  const { config, toggleAudioOutput } = useContext(RuntimeStateAPIContext);
+  const state = useContext(RuntimeStateContext);
+
+  const handleClose = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    if (hasEnded) {
+      onEnd?.(event);
+    } else {
+      setAlert(true);
+    }
+  };
+
+  const handleResume = (): void => setAlert(false);
+
+  const headerActions = useMemo<HeaderActionProps[]>(() => {
+    const items: HeaderActionProps[] = [{ svg: 'close', onClick: handleClose }];
+
+    if (config.render?.mode === RenderMode.OVERLAY) {
+      items.unshift({ svg: 'minus', onClick: onMinimize });
+    }
+
+    if (audioInterface) {
+      items.unshift({
+        svg: state.audioOutput ? 'sound' : 'soundOff',
+        onClick: toggleAudioOutput,
+      });
+    }
+
+    return items;
+  }, [config.render, handleClose, onMinimize, state.audioOutput, audioInterface]);
 
   const scrollableAreaRef = useRef<HTMLDivElement>(null);
 
@@ -79,7 +117,7 @@ export const NewChat: React.FC<INewChat> = ({
 
   return (
     <div className={clsx(ClassName.CHAT, chatContainer)} onKeyDown={handleKeyDown}>
-      <Header title={title} image={mockAvatar} />
+      <Header title={title} image={mockAvatar} actions={headerActions} />
       <div ref={scrollableAreaRef} className={dialogContainer}>
         <WelcomeMessage title={title} description={description} avatar={avatar} />
         {children}
@@ -92,6 +130,11 @@ export const NewChat: React.FC<INewChat> = ({
         extraLinkText={extraLinkText}
         extraLinkUrl={extraLinkUrl}
         messageInputProps={{ ...messageInputProps }}
+      />
+      <Prompt
+        visible={hasAlert}
+        accept={{ label: 'End Chat', /* type: 'warn', */ onClick: chain(onEnd, handleResume) }}
+        cancel={{ label: 'Cancel', onClick: handleResume }}
       />
     </div>
   );
