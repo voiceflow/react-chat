957e096bffbcf4221c13385c38091220af206393
Mikaal Naik <matthewnaik@gmail.com>
2024-10-30 15:24:37 -0400

    feat: working embedded chat kit

--- apps/documentation/src/components/Chat/index.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-
-import { ChatWidget } from '@voiceflow/chat';
-export const ChatComponent = () => {
-  return <div>{globalThis.window && <ChatWidget chatAPI={undefined} />}</div>;
-};
