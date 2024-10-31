60d582d006510dabd2ba2178ffbae3889b2d1e91
Mikaal Naik <matthewnaik@gmail.com>
2024-10-31 09:11:21 -0400

    feat: ability to update projectID

--- apps/documentation/src/components/ChatConfigOptions/index.tsx
+++ apps/documentation/src/components/ChatConfigOptions/index.tsx
@@ -1,11 +1,27 @@
-import { useState } from 'react';
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { ChatScript } from '../ChatScript';
 
 export const ChatConfigOptions = () => {
   const [projectID, setProjectID] = useState<string>('');
 
+  useEffect(() => {
+    const storedProjectID = localStorage.getItem('projectID') || '';
+    setProjectID(storedProjectID);
+  }, []);
+
+  const handleSave = () => {
+    localStorage.setItem('projectID', projectID);
+    window.location.reload();
+  };
+
   return (
     <div>
       <input type="text" value={projectID} placeholder="ProjectID" onChange={(e) => setProjectID(e.target.value)} />
+      <button onClick={handleSave}>Save</button>
+      <ChatScript projectID={projectID} />
     </div>
   );
 };
--- apps/documentation/src/pages/_document.tsx
+++ apps/documentation/src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+import { ChatScript } from '@/components/ChatScript';
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -8,28 +10,7 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-      <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `
-              (function (d, t) {
-                const v = d.createElement(t);
-                const s = d.getElementsByTagName(t)[0];
-                v.onload = function () {
-                  window.voiceflow.chat.load({
-                    verify: { projectID: '643b0fec8f78850008907e2f' },
-                     assistant: {
-                      stylesheet: '/dist/style.css',
-                    }
-                   });
-                };
-                v.src = '/dist/bundle.mjs';
-                v.type = 'text/javascript';
-                s.parentNode.insertBefore(v, s);
-              })(document, 'script');
-            `,
-        }}
-      />
+      <ChatScript projectID="643b0fec8f78850008907e2f" />
     </Html>
   );
 }
