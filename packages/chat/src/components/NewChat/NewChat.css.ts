54a990ad1efe7f908fea73e8ce234b06c1ef541c
Mikaal Naik <matthewnaik@gmail.com>
2024-10-31 14:59:57 -0400

    fix: border

--- packages/chat/src/components/NewChat/NewChat.css.ts
+++ packages/chat/src/components/NewChat/NewChat.css.ts
@@ -10,7 +10,6 @@ export const chatContainer = style({
   flexDirection: 'column',
   borderRadius: '16px',
   backgroundColor: COLORS.white,
-  border: `.5px solid ${COLORS.NEUTRAL_LIGHT[100]}`,
   position: 'relative',
   boxShadow:
     '0px 0px 0px 1px rgba(22, 26, 30, 0.06), 0px 1px 1px 0px rgba(22, 26, 30, 0.04), 0px 4px 8px -32px rgba(22, 26, 30, 0.04), 0px 10px 16px -32px rgba(22, 26, 30, 0.06), 0px 16px 20px -32px rgba(22, 26, 30, 0.08), 0px 24px 32px -32px rgba(22, 26, 30, 0.08), 0px 32px 40px -32px rgba(22, 26, 30, 0.10), 0px 40px 64px -32px rgba(22, 26, 30, 0.12)',
