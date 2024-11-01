700f7974835d2a270cff14caf0e22a0ea205883e
Gilly Barr <gillyb@gmail.com>
2024-11-01 14:18:59 -0400

    feat: ui review for carousel component

--- packages/chat/src/components/Carousel/Carousel.story.tsx
+++ packages/chat/src/components/Carousel/Carousel.story.tsx
@@ -1,13 +1,58 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Avatar } from '@/components/Avatar';
-import { MOCK_IMAGE, VF_ICON } from '@/fixtures';
+import { RuntimeProvider } from '@/contexts';
+import { MOCK_IMAGE } from '@/fixtures';
+import { DEFAULT_AVATAR, RenderMode } from '@/main';
+import { WithDefaultPalette } from '@/storybook/decorators';
+import { COLORS } from '@/styles/colors';
+import { ChatPersistence, ChatPosition } from '@/types';
 
+import { NewChat } from '../NewChat';
+import { MessageType } from '../SystemResponse/constants';
+import { SystemMessage } from '../SystemResponse/SystemMessage';
 import { Carousel } from '.';
 
 const meta: Meta<typeof Carousel> = {
   component: Carousel,
   title: 'Components/Carousel',
+  decorators: [
+    (Story) => (
+      <RuntimeProvider
+        config={{
+          verify: { projectID: 'project-id' },
+          url: '',
+          versionID: 'version-id',
+          autostart: true,
+          allowDangerousHTML: true,
+          user: { name: 'User' },
+          render: { mode: RenderMode.OVERLAY },
+        }}
+        assistant={{
+          title: 'Voiceflow Assistant',
+          color: COLORS.ACCENT[500],
+          image: DEFAULT_AVATAR,
+          avatar: DEFAULT_AVATAR,
+          launcher: undefined,
+          watermark: true,
+          feedback: false,
+          stylesheet: undefined,
+          description: '',
+          position: ChatPosition.RIGHT,
+          persistence: ChatPersistence.LOCAL_STORAGE,
+          audioInterface: false,
+          defaultAudioOutput: undefined,
+          spacing: {
+            side: 30,
+            bottom: 30,
+          },
+          extensions: [],
+        }}
+      >
+        {Story()}
+      </RuntimeProvider>
+    ),
+    WithDefaultPalette,
+  ],
 };
 type Story = StoryObj<typeof Carousel>;
 
@@ -43,6 +88,39 @@ const MULTIPLE_CARDS = [
   },
 ];
 
+export const Default: Story = {
+  render: () => (
+    <div style={{ width: '380px' }}>
+      <NewChat
+        title="Your awesome assistant"
+        description="Im hot, youre not, deal with it"
+        avatar={DEFAULT_AVATAR}
+        isLoading={false}
+        image={MOCK_IMAGE}
+        hasEnded={false}
+        messageInputProps={{}}
+      >
+        <SystemMessage
+          avatar={DEFAULT_AVATAR}
+          message={{
+            type: MessageType.CAROUSEL,
+            cards: MULTIPLE_CARDS,
+          }}
+          withImage={false}
+        />
+        <SystemMessage
+          avatar={DEFAULT_AVATAR}
+          message={{
+            type: MessageType.TEXT,
+            text: 'Do you like this carousel ?',
+          }}
+          withImage={true}
+        />
+      </NewChat>
+    </div>
+  ),
+};
+
 export const SingleCard: Story = {
   args: {
     cards: [FIRST_CARD],
@@ -55,20 +133,20 @@ export const MultipleCards: Story = {
   },
 };
 
-export const ControlsTemplate: Story = {
-  args: {
-    cards: MULTIPLE_CARDS,
-  },
+// export const ControlsTemplate: Story = {
+//   args: {
+//     cards: MULTIPLE_CARDS,
+//   },
 
-  render: (args) => {
-    return (
-      <>
-        <Avatar avatar={VF_ICON} />
-        <Carousel {...args} />
-      </>
-    );
-  },
-};
+//   render: (args) => {
+//     return (
+//       <>
+//         <Avatar avatar={VF_ICON} />
+//         <Carousel {...args} />
+//       </>
+//     );
+//   },
+// };
 
 export const WithControls: Story = {
   args: {
