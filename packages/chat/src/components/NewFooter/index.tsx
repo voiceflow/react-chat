0831a3edbdc1b2f808c59237e595150e0f7df2ac
Mikaal Naik <matthewnaik@gmail.com>
2024-11-07 13:05:56 -0500

    feat: powered by Voiceflow is a link now

--- packages/chat/src/components/NewFooter/index.tsx
+++ packages/chat/src/components/NewFooter/index.tsx
@@ -13,7 +13,10 @@ import {
   footerLinksContainer,
   messageContainer,
   separator,
+  voiceflowLink,
 } from './NewFooter.css';
+
+const VOICEFLOW_URL = 'https://www.voiceflow.com/';
 
 export interface INewFooter {
   buttons?: { label: string; onClick: () => void }[];
@@ -66,7 +69,14 @@ export const NewFooter: React.FC<INewFooter> = ({
           <MessageInput {...messageInputProps} />
         )}
         <div className={footerLinksContainer}>
-          {showPoweredBy && <div>Powered by Voiceflow</div>}
+          {showPoweredBy && (
+            <div>
+              Powered by{' '}
+              <a href={VOICEFLOW_URL} className={voiceflowLink}>
+                Voiceflow
+              </a>
+            </div>
+          )}
           {showPoweredBy && showExtraLink && <div className={separator} />}
           {showExtraLink && (
             <a href={extraLinkUrl} target="_blank" className={extraLinkStyles}>
