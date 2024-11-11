332a555ca9ecc67245d072db46a9e511e6605346
Gilly Barr <gillyb@gmail.com>
2024-11-11 10:52:45 -0500

    fix: remove bundled package from git

--- apps/documentation/src/scripts/import-chat.sh
+++ apps/documentation/src/scripts/import-chat.sh
@@ -6,8 +6,9 @@ yarn install
 yarn build
 
 # Copy bundled files
-cp dist/bundle.mjs ../../apps/documentation/public/bundle/bundle.mjs
-cp dist/style.css ../../apps/documentation/public/bundle/style.css
+mkdir ../../apps/documentation/public/bundle
+cp ./dist/bundle.mjs ../../apps/documentation/public/bundle/bundle.mjs
+cp ./dist/style.css ../../apps/documentation/public/bundle/style.css
 
 # Go back to previous working directory
 cd ../../packages/chat
