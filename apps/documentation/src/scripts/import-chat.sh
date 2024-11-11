ed428f2b894b0810ee712b06154af8d424ec9285
Gilly Barr <gillyb@gmail.com>
2024-11-11 10:46:24 -0500

    feat: import built chat instead of building ourselves (COR-0000)

---
+++ apps/documentation/src/scripts/import-chat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build chat package
+cd ../../packages/chat
+yarn install
+yarn build
+
+# Copy bundled files
+cp dist/bundle.mjs ../../apps/documentation/public/bundle/bundle.mjs
+cp dist/style.css ../../apps/documentation/public/bundle/style.css
+
+# Go back to previous working directory
+cd ../../packages/chat
