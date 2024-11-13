0018677da1d3e683fa1cb0ad5ce5006ac57ddb30
Mikaal Naik <matthewnaik@gmail.com>
2024-11-13 17:17:08 -0500

    fix: themed feedback button

--- packages/chat/src/components/FeedbackButton/FeedbackButton.story.tsx
+++ packages/chat/src/components/FeedbackButton/FeedbackButton.story.tsx
@@ -1,4 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { assignInlineVars } from '@vanilla-extract/dynamic';
+
+import { createPalette } from '@/styles/colors';
+import { PALETTE } from '@/styles/colors.css';
 
 import { FeedbackButton } from '.';
 import { FeedbackButtonVariant } from './FeedbackButton.interface';
@@ -13,9 +17,17 @@ export default meta;
 type Story = StoryObj<typeof FeedbackButton>;
 
 export const PreviousResponse: Story = {
-  render: () => <FeedbackButton onClick={() => null} variant={FeedbackButtonVariant.PREVIOUS_RESPONSE} />,
+  render: () => (
+    <div style={{ width: '400px', ...assignInlineVars(PALETTE, { colors: createPalette('green') }) }}>
+      <FeedbackButton onClick={() => null} variant={FeedbackButtonVariant.PREVIOUS_RESPONSE} />
+    </div>
+  ),
 };
 
 export const LastResponse: Story = {
-  render: () => <FeedbackButton onClick={() => null} variant={FeedbackButtonVariant.LAST_RESPONSE} />,
+  render: () => (
+    <div style={{ width: '400px', ...assignInlineVars(PALETTE, { colors: createPalette('red') }) }}>
+      <FeedbackButton onClick={() => null} variant={FeedbackButtonVariant.LAST_RESPONSE} />
+    </div>
+  ),
 };
