5e058047a6ca2c3f5348237fc81768fb016f08b4
Mikaal Naik <matthewnaik@gmail.com>
2024-11-19 10:46:48 -0500

    fix: carousel background against avatar

--- packages/chat/src/components/Carousel/index.tsx
+++ packages/chat/src/components/Carousel/index.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 import { ClassName } from '@/constants';
 import { fadeInAndUp } from '@/styles/animation-utils.css';
@@ -9,7 +9,14 @@ import { CARD_WIDTH } from '../Card/styles.css';
 import type { CardProps } from '../Card/types';
 import { CarouselButton } from './CarouselButton';
 import { useScrollObserver, useScrollTo } from './hooks';
-import { cardsContainer, cardsInnerContainer, carouselContainer, GUTTER_WIDTH, lastCardSpacer } from './styles.css';
+import {
+  cardsContainer,
+  cardsInnerContainer,
+  carouselContainer,
+  fauxBackground,
+  GUTTER_WIDTH,
+  lastCardSpacer,
+} from './styles.css';
 
 const CARD_WITH_GUTTER = CARD_WIDTH + GUTTER_WIDTH;
 
@@ -28,29 +35,41 @@ export interface CarouselProps {
 export const Carousel: React.FC<CarouselProps> = ({ cards }) => {
   const scrollContainerRef = useRef<HTMLDivElement>(null);
   const { showPreviousButton, showNextButton } = useScrollObserver(scrollContainerRef, cards);
-
+  const [cardIndex, setCardIndex] = useState(0);
   const scrollToPrevious = useScrollTo(scrollContainerRef, (el) => Math.ceil(el.scrollLeft / CARD_WITH_GUTTER) - 1);
   const scrollToNext = useScrollTo(scrollContainerRef, (el) => Math.floor(el.scrollLeft / CARD_WITH_GUTTER) + 1);
 
+  const handleScrollToNext = () => {
+    scrollToNext();
+    setCardIndex((prev) => prev + 1);
+  };
+
+  const handleScrollToPrevious = () => {
+    scrollToPrevious();
+    setCardIndex((prev) => prev - 1);
+  };
+
   return (
     <div className={clsx(ClassName.CAROUSEL, carouselContainer)}>
+      <div className={fauxBackground({ afterFirstCard: cardIndex > 0 })} />
       <div ref={scrollContainerRef} className={cardsContainer}>
         <div className={cardsInnerContainer}>
           {cards.map((card, i) => (
             <div
               className={fadeInAndUp}
+              key={i}
               style={{
                 animationDelay: `${i * 0.1}s`,
               }}
             >
-              <Card {...card} key={i} />
+              <Card {...card} />
             </div>
           ))}
           <div className={lastCardSpacer}> </div>
         </div>
       </div>
-      <CarouselButton direction="left" visible={showPreviousButton} onClick={scrollToPrevious} />
-      <CarouselButton direction="right" visible={showNextButton} onClick={scrollToNext} />
+      <CarouselButton direction="left" visible={showPreviousButton} onClick={handleScrollToPrevious} />
+      <CarouselButton direction="right" visible={showNextButton} onClick={handleScrollToNext} />
     </div>
   );
 };
