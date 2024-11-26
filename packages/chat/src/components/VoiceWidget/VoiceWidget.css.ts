934befe65754bcc776cd91d95dd436e8485206f2
Mikaal Naik <matthewnaik@gmail.com>
2024-11-26 16:04:19 -0500

    feat: expanded variant

--- packages/chat/src/components/VoiceWidget/VoiceWidget.css.ts
+++ packages/chat/src/components/VoiceWidget/VoiceWidget.css.ts
@@ -14,29 +14,60 @@ export const voiceWrapper = style({
   width: 'fit-content',
 });
 
-export const voiceWidgetContainer = style({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  padding: '16px 20px',
-  background: 'white',
-  borderRadius: '16px',
-  boxShadow: SHADOWS.Z64_Light,
-  overflow: 'hidden',
-  width: 'fit-content',
-  position: 'relative',
-  zIndex: 1,
-  userSelect: 'none',
-  gap: '16px',
+export const voiceWidgetContainer = recipe({
+  base: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '16px 20px',
+    background: 'white',
+    borderRadius: '16px',
+    boxShadow: SHADOWS.Z64_Light,
+    overflow: 'hidden',
+    width: 'fit-content',
+    position: 'relative',
+    zIndex: 1,
+    userSelect: 'none',
+  },
+  variants: {
+    type: {
+      full: {},
+      expanded: {
+        flexDirection: 'column',
+        padding: '32px 24px 24px 24px',
+      },
+      compact: {
+        borderRadius: '100px',
+        gap: '8px',
+        padding: '8px',
+      },
+    },
+  },
 });
 
-export const controlSection = style({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-  gap: '8px',
-  width: '100%',
-  boxSizing: 'border-box',
+export const controlSection = recipe({
+  base: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    gap: '8px',
+    width: '100%',
+  },
+  variants: {
+    type: {
+      full: {
+        gap: '8px',
+      },
+      expanded: {
+        alignItems: 'center',
+        paddingTop: '12px',
+        gap: '12px',
+      },
+      compact: {
+        gap: '4px',
+      },
+    },
+  },
 });
 
 export const titleStyle = style({
@@ -49,28 +80,66 @@ export const titleStyle = style({
   textAlign: 'center',
 });
 
-export const buttonModifier = style({
-  // display: 'flex',
-  // justifyContent: 'center',
-  // alignItems: 'center',
-  width: '158px',
-  position: 'relative',
-  borderRadius: '8px',
-  padding: '16px 4px',
-  // gap: '6px',
+export const buttonModifier = recipe({
+  base: {
+    width: '158px',
+    position: 'relative',
+    borderRadius: '8px',
+    padding: '16px 4px',
+  },
+  variants: {
+    type: {
+      full: {},
+      expanded: {
+        width: '182px',
+      },
+      compact: {
+        borderRadius: '100px',
+        width: '194px',
+      },
+    },
+  },
 });
 
-export const circle = style({
-  // height: '54px',
-  // width: '54px',
-  // minWidth: '54px',
-  // minHeight: '54px',
-  // borderRadius: '50%',
-  // overflow: 'hidden',
-  // background: 'linear-gradient(270deg, #ff6ec4, #7873f5, #4bc0c8)',
-  // backgroundSize: '200% 200%',
-  // animation: `${gradientAnimation} 4s ease infinite`,
-  // animationDuration: '4s',
+export const circle = recipe({
+  base: {
+    borderRadius: '50%',
+    height: '54px',
+    width: '54px',
+    minWidth: '54px',
+    minHeight: '54px',
+  },
+  variants: {
+    type: {
+      full: {
+        height: '54px',
+        width: '54px',
+        minWidth: '54px',
+        minHeight: '54px',
+        marginRight: '16px',
+      },
+      expanded: {
+        height: '80px',
+        width: '80px',
+        minWidth: '80px',
+        minHeight: '80px',
+        marginBottom: '8px',
+      },
+      compact: {
+        height: '32px',
+        width: '32px',
+        minWidth: '32px',
+        minHeight: '32px',
+      },
+    },
+  },
+});
+
+export const imageStyles = style({
+  borderRadius: '50%',
+  height: '100%',
+  width: '100%',
+  objectFit: 'cover',
 });
 
 export const buttonContent = recipe({
@@ -103,4 +172,8 @@ export const buttonContent = recipe({
 
 export const buttonText = style({
   paddingTop: '2px',
+});
+
+export const linkSectionModifier = style({
+  paddingBottom: 0,
 });
