92fd59a302053685e437bccaadd0e4961ecae36a
Mikaal Naik <matthewnaik@gmail.com>
2024-11-26 14:03:29 -0500

    feat: base looking good. dropped experimental work

--- packages/chat/src/components/VoiceWidget/index.tsx
+++ packages/chat/src/components/VoiceWidget/index.tsx
@@ -1,14 +1,59 @@
+import { useState } from 'react';
+
 import { Button } from '../Button';
+import { ButtonIcon } from '../Button/ButtonIcon';
 import { ButtonVariant } from '../Button/constants';
-import { controlSection, titleStyle, voiceWidgetContainer } from './VoiceWidget.css';
+import {
+  buttonContent,
+  buttonModifier,
+  buttonText,
+  circle,
+  controlSection,
+  titleStyle,
+  voiceWidgetContainer,
+} from './VoiceWidget.css';
 
 export const VoiceWidget = ({ children }: { children: React.ReactNode }) => {
+  const [isCalling, setIsCalling] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsCalling((prev) => !prev);
+    if (isCalling) {
+      endCall();
+      // End call
+    } else {
+      startCall();
+      // Start call
+    }
+  };
+
+  const startCall = () => {
+    // Start call
+  };
+
+  const endCall = () => {
+    // End call
+  };
+
   return (
     <div className={voiceWidgetContainer}>
-      {children}
+      <div className={circle}>{children}</div>
       <div className={controlSection}>
         <div className={titleStyle}>How can I help you?</div>
-        <Button variant={ButtonVariant.PRIMARY}>Start</Button>
+        <Button
+          onClick={handleButtonClick}
+          variant={isCalling ? ButtonVariant.SECONDARY : ButtonVariant.PRIMARY}
+          className={buttonModifier}
+        >
+          <span className={buttonContent({ isVisible: isCalling })}>
+            <ButtonIcon svg="endCall" />
+            <div className={buttonText}>End</div>
+          </span>
+          <span className={buttonContent({ isVisible: !isCalling })}>
+            <ButtonIcon svg="phone" />
+            <div className={buttonText}>Start a call</div>
+          </span>
+        </Button>
       </div>
     </div>
   );
