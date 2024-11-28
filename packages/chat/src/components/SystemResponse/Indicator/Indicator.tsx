10c1ba419255e5a8057b8fb52a37884616b4588d
Mikaal Naik <matthewnaik@gmail.com>
2024-11-28 15:49:48 -0500

    fix: match the message layout in indicator

--- packages/chat/src/components/SystemResponse/Indicator/Indicator.tsx
+++ packages/chat/src/components/SystemResponse/Indicator/Indicator.tsx
@@ -4,6 +4,7 @@ import { Avatar } from '@/components/Avatar';
 import { MessageContainer } from '@/components/MessageContainer';
 import { TypingIndicator } from '@/components/TypingIndicator';
 
+import { responseAvatar } from '../styles.css';
 import { indicatorContainer } from './Indicator.css';
 
 export interface IndicatorProps {
@@ -14,7 +15,7 @@ export interface IndicatorProps {
 
 const Indicator: React.FC<IndicatorProps> = ({ avatar, isLast, className }) => (
   <MessageContainer className={clsx(indicatorContainer, className)} isLast={isLast}>
-    <Avatar avatar={avatar} />
+    <Avatar avatar={avatar} className={responseAvatar} />
     <TypingIndicator />
   </MessageContainer>
 );
