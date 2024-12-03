f007f0684bea068d95171cc00f77cf2f32fa2fad
Gilly Barr <gillyb@gmail.com>
2024-12-03 14:27:36 -0500

    feat: add popover modality (COR-0000)

--- packages/chat/src/views/ChatWidget/styles.css.ts
+++ packages/chat/src/views/ChatWidget/styles.css.ts
@@ -4,6 +4,9 @@ import { recipe } from '@vanilla-extract/recipes';
 export const CHAT_WIDTH = 400;
 const MAX_CHAT_HEIGHT = 800;
 
+const POPOVER_WIDTH = 750;
+const POPOVER_HEIGHT = 400;
+
 export const LAUNCHER_MARGIN = 16;
 
 const WIDGET_Z_INDEX = 10000;
@@ -29,27 +32,65 @@ export const widgetContainer = recipe({
 export const chatIsOpen = widgetContainer.classNames.variants.withChat.true;
 export const chatIsClosed = widgetContainer.classNames.variants.withChat.false;
 
-export const chatContainer = style({
-  width: CHAT_WIDTH,
-  maxHeight: MAX_CHAT_HEIGHT,
-  pointerEvents: 'auto',
+export const chatContainer = recipe({
+  base: {
+    width: CHAT_WIDTH,
+    maxHeight: MAX_CHAT_HEIGHT,
+    pointerEvents: 'auto',
 
-  selectors: {
-    [`${widgetContainer.classNames.base} &`]: {
-      position: 'absolute',
-    },
+    selectors: {
+      [`${widgetContainer.classNames.base} &`]: {
+        position: 'absolute',
+      },
 
-    [`.${widgetContainer.classNames.variants.withChat.true} &`]: {
-      opacity: 1,
-      pointerEvents: 'auto',
-      transform: 'translateY(0%)',
-      transition: 'transform 300ms cubic-bezier(0, 0.95, 0.1, 1), opacity 150ms linear',
+      [`.${widgetContainer.classNames.variants.withChat.true} &`]: {
+        opacity: 1,
+        pointerEvents: 'auto',
+        transform: 'translateY(0%)',
+        transition: 'transform 300ms cubic-bezier(0, 0.95, 0.1, 1), opacity 150ms linear',
+      },
+      [`.${widgetContainer.classNames.variants.withChat.false} &`]: {
+        opacity: 0,
+        pointerEvents: 'none',
+        transform: 'translateY(100%)',
+        transition: 'transform 300ms cubic-bezier(0.85, 0, 0.6, 1), opacity 150ms linear',
+      },
     },
-    [`.${widgetContainer.classNames.variants.withChat.false} &`]: {
-      opacity: 0,
-      pointerEvents: 'none',
-      transform: 'translateY(100%)',
-      transition: 'transform 300ms cubic-bezier(0.85, 0, 0.6, 1), opacity 150ms linear',
+  },
+
+  variants: {
+    popover: {
+      true: {
+        height: POPOVER_HEIGHT,
+        maxHeight: POPOVER_HEIGHT,
+        width: POPOVER_WIDTH,
+        left: `calc(50% - ${POPOVER_WIDTH / 2}px)`,
+        top: 42,
+      },
+    },
+  },
+});
+
+export const popoverBackdrop = recipe({
+  base: {
+    display: 'none',
+  },
+  variants: {
+    visible: {
+      true: {
+        display: 'block',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        width: '100%',
+        height: '100%',
+        backdropFilter: 'blur(4px)',
+        filter: 'blur(4px)',
+        backgroundColor: 'rgba(0, 0, 0, 0.16)',
+        zIndex: 10,
+        pointerEvents: 'all',
+      },
     },
   },
 });
