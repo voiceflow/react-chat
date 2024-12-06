8adbf92021ecda347405e738d7bf0eb7fcdd7211
Mikaal Naik <matthewnaik@gmail.com>
2024-12-06 16:30:18 -0500

    fix: svg syntax and inheritance

--- packages/chat/src/components/Launcher/ChevronIcon.tsx
+++ packages/chat/src/components/Launcher/ChevronIcon.tsx
@@ -1,11 +1,11 @@
 import type { SVGProps } from 'react';
 export const ChevronIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" {...props}>
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M4.29289 9.29289C4.68342 8.90237 5.31658 8.90237 5.70711 9.29289L12 15.5858L18.2929 9.29289C18.6834 8.90237 19.3166 8.90237 19.7071 9.29289C20.0976 9.68342 20.0976 10.3166 19.7071 10.7071L12.7071 17.7071C12.3166 18.0976 11.6834 18.0976 11.2929 17.7071L4.29289 10.7071C3.90237 10.3166 3.90237 9.68342 4.29289 9.29289Z"
-      fill="#E7F5FD"
+      fill="currentColor"
     />
   </svg>
 );
